50ca69b149e25c38b293ddfcb8828806f377b84b
李旭 <lixu@lixudeMacBook-Air.local>
2023-04-01 08:54:08 +0800

    add selection sort

---
+++ src/selection.py
@@ -0,0 +1,19 @@
+from sort import Sort
+
+class SelectionSort(Sort):
+    @classmethod
+    def sort(cls, arr: list):
+        for i in range(len(arr) - 1):
+            min_idx = i
+            for j in range(i, len(arr)):
+                if arr[min_idx] > arr[j]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        
+        assert cls.is_sorted(arr)
+        return arr
+    
+
+if __name__ == '__main__':
+    SelectionSort.main()
+            

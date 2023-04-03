50d506f4d1276321b25f3776b763738ecc0ecee2
李旭 <lixu@lixudeMacBook-Air.local>
2023-04-03 09:57:55 +0800

    add insertion sort

---
+++ src/insertion.py
@@ -0,0 +1,18 @@
+from sort import Sort
+
+
+class Insertion(Sort):
+    @classmethod
+    def sort(cls, arr):
+        for i in range(1, len(arr)):
+            for j in range(i, 0, -1):
+                if arr[j-1] > arr[j]:
+                    arr[j-1], arr[j] = arr[j], arr[j-1]
+                else:
+                    break
+        assert cls.is_sorted(arr)
+        return arr
+
+
+if __name__ == '__main__':
+    Insertion.main()
